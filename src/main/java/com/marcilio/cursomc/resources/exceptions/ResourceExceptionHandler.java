052da9b589b2cf7aafc654b67a43e09119f23f3e
marciliopaneto <marciliopaneto@yahoo.com.br>
2019-08-15 09:15:10 -0300

    ajuste no endpoint /categorias/{id}

---
+++ src/main/java/com/marcilio/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.marcilio.cursomc.resources.exceptions;
+
+import com.marcilio.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+        StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(err);
+    }
+}
--- src/main/java/com/marcilio/cursomc/services/CategoriaService.java
+++ src/main/java/com/marcilio/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.marcilio.cursomc.services;
 
 import com.marcilio.cursomc.domain.Categoria;
 import com.marcilio.cursomc.repositories.CategoriaRepository;
+import com.marcilio.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,10 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id) {
         Categoria obj = categoriaRepository.findOne(id);
+        if (obj == null) {
+            throw new ObjectNotFoundException("Objeto não encontrado Id: " + id
+                + ", tipo: " + Categoria.class.getName());
+        }
         return obj;
     }
 
