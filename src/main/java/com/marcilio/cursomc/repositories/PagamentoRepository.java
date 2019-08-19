91992c8b26ac14e1e89c58b1b90aeb8d11b9a243
marciliopaneto <marciliopaneto@yahoo.com.br>
2019-08-19 17:48:31 -0300

    Pedido, Pagamento e Estado de Pagamento

---
+++ src/main/java/com/marcilio/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,10 @@
+package com.marcilio.cursomc.repositories;
+
+import com.marcilio.cursomc.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
